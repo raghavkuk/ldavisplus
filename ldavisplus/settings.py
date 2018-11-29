eeb310df65ebbabe0c4c0c8f63598e58f4d60051
Rohan Passi <rohanpassi94@gmail.com>
2018-11-28 20:18:56 -0700

    changes for deploy

--- ldavisplus/settings.py
+++ ldavisplus/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dx3jilcv9rbzmyu7d*qb$%jn7_pej$o**8vqzn950=kw8^%nnp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
